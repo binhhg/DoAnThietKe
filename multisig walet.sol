e9adb702a74ec8754148b96a0a05bf11ee279e75
binhhg <binhvxhg789@gmail.com>
2022-06-02 23:24:19 +0700

    'BE'

---
+++ multisig walet.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.7.0 <0.9.0;
+
+contract MultiSigWallet {
+    event Deposit(address indexed sender, uint amount);
+    event Submit(uint indexed txId);
+    event Approve(address indexed owner, uint indexed txId);
+    event Revoke(address indexed owner, uint indexed txId);
+    event Execute(uint indexed txId);
+
+    struct Transaction {
+        address to;
+        uint value;
+        bytes data;
+        bool executed;
+    }
+
+    address[] public owners;
+    mapping(address => bool) public isOwner;
+    uint public required;
+
+    Transaction[] public transactions;
+    // mapping from tx id => owner => bool
+    mapping(uint => mapping(address => bool)) public approved;
+
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "not owner");
+        _;
+    }
+
+    modifier txExists(uint _txId) {
+        require(_txId < transactions.length, "tx does not exist");
+        _;
+    }
+
+    modifier notApproved(uint _txId) {
+        require(!approved[_txId][msg.sender], "tx already approved");
+        _;
+    }
+
+    modifier notExecuted(uint _txId) {
+        require(!transactions[_txId].executed, "tx already executed");
+        _;
+    }
+
+    constructor(address[] memory _owners, uint _required) {
+        require(_owners.length > 0, "owners required");
+        require(
+            _required > 0 && _required <= _owners.length,
+            "invalid required number of owners"
+        );
+
+        for (uint i; i < _owners.length; i++) {
+            address owner = _owners[i];
+
+            require(owner != address(0), "invalid owner");
+            require(!isOwner[owner], "owner is not unique");
+
+            isOwner[owner] = true;
+            owners.push(owner);
+        }
+
+        required = _required;
+    }
+}
